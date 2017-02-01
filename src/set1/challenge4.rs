d91362fee6ee7d91b4c3c74f9f1fdc6bc810cdcc
Robert Kuska <rkuska@gmail.com>
2017-02-01 19:31:06 +0100

    set1: Remove unecessary unwrap

--- src/set1/challenge4.rs
+++ src/set1/challenge4.rs
@@ -4,13 +4,13 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 
-fn find_line(path: &str) -> String {
+fn find_line(path: &str) -> Option<(f32, String)> {
     let file = BufReader::new(File::open(path).expect("Error when reading file <challenge 4>"));
-    file.lines().filter_map(|x| find_original(&hex_to_bytes(&x.unwrap()))).min_by(|a, b| (a.0).partial_cmp(&b.0).unwrap()).unwrap().1
+    file.lines().filter_map(|x| find_original(&hex_to_bytes(&x.unwrap()))).min_by(|a, b| (a.0).partial_cmp(&b.0).unwrap())
 }
 
 
 #[test]
 fn test_find_line(){
-    assert_eq!(find_line("./files/challenge-4.txt"), "Now that the party is jumping\n")
+    assert_eq!(find_line("./files/challenge-4.txt").unwrap().1, "Now that the party is jumping\n")
 }
